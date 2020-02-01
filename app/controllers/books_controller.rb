0c0b693a3e4e297b2af2277139520e59a6f96918
tama <yuki023201025@gmail.com>
2020-02-01 17:10:25 +0900

    validationと画像の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,12 @@ class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		@book.save
+		if @book.save
 		redirect_to book_path(@book.id)
+		else
+			@books = Book.all
+			render :index
+		end
 	end
 
 	def destroy
@@ -32,9 +36,12 @@ class BooksController < ApplicationController
 
 	def update
 	 	@book = Book.find(params[:id])
-		@book.update(book_params)
+		if @book.update(book_params)
 		#flash[:notice] = "successfully Updated"
 		redirect_to book_path(@book.id)
+		else
+			render :edit
+		end
 	end
 
 	private
